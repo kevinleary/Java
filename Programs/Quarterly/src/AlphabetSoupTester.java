564c92d245ff36878e02fe437c5d6f4c9df04435
kevinleary <kevin.n.leary@gmail.com>
2014-11-13 21:28:46 -0500

    Quarterly V1

---
+++ Programs/Quarterly/src/AlphabetSoupTester.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/*
+
+Kevin Leary
+Period 2
+Date: 11/12/14
+Excercise: QUARTERLY
+
+*/
+public class AlphabetSoupTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
