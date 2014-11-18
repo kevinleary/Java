2507151e8a0fb5400f432129441a4c1a8bd2a488
Kevin Leary <kevin.n.leary@gmail.com>
2014-11-17 23:03:57 -0500

    7-15

---
+++ Programs/Chapter 7/7-15/src/Order.java
@@ -0,0 +1,16 @@
+
+public class Order {
+	
+	public static double getOrderTotal(int bp, int nb){
+		
+		double bpPrice = 18.95;
+		double nbPrice = 21.95;
+		
+		if (bp == 1){
+			
+		}
+		
+		
+		
+	}
+}
